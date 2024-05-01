29e23af80dd7a84cadb3ec87d37841175edf36a4
ALI IMBIRIKA <160365691+imbirikaa@users.noreply.github.com>
2024-05-02 02:35:57 +0300

    Delete dataImg directory

--- dataImg/Letters.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Letters" tilewidth="8" tileheight="10" tilecount="50" columns="10">
- <image source="../../../Pixel Adventure 1/Free/Menu/Text/Text (Black) (8x10).png" width="80" height="50"/>
-</tileset>
